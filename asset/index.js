925ccf6856ee7608b30401da5e4719a505f3c479
HoangTran <99.hoangtran@gmail.com>
2020-12-14 16:36:39 +0700

    [Fix] asset path

--- asset/index.js
+++ asset/index.js
@@ -5,7 +5,7 @@ const assetPaths = [
     "asset/image/champion/ahri/Ahri.avatar.circle.png",
     "asset/image/champion/ahri/Ahri.avatar.square.png",
     "asset/image/champion/ahri/Charm.ability.png",
-    "asset/image/champion/ahri/Fox-fire.ability.png",
+    "asset/image/champion/ahri/Fox-Fire.ability.png",
     "asset/image/champion/ahri/Orb-of-Deception.ability.png",
     "asset/image/champion/ahri/Spirit-Rush.ability.png",
 
