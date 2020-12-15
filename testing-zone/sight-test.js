03770d202ac495e2c9514a47157c8c2c1e030da8
HoangTran <99.hoangtran@gmail.com>
2020-12-15 09:47:56 +0700

    [Update] compare blendMode and erase

--- testing-zone/sight-test.js
+++ testing-zone/sight-test.js
@@ -1,52 +1,126 @@
 // live preview: https://editor.p5js.org/HoangTran0410/sketches/7Dlqz7z77
 
 let overlay;
-let overlayColor = "#000d";
-let groundColor = "green";
+let overlayColor = "#000b";
+let groundColor = "#1af";
 let champColor = "blue";
-let fovRadius = 200;
+let fovRadius = 250;
+let useBlendMode = false;
+let things = [];
 
 function setup() {
-    createCanvas(400, 400);
-    overlay = createGraphics(400, 400);
+    createCanvas(600, 600);
+    overlay = createGraphics(600, 600);
+    overlay.noStroke();
 
     angleMode(DEGREES);
+
+    for (let i = 0; i < 15; i++) {
+        things.push({
+            pos: createVector(random(width), random(height)),
+            vel: createVector(random(-3, 3), random(-3, 3)),
+            color: [random(255), random(255), random(255)],
+            r: random(10, 50),
+        });
+    }
 }
 
 function draw() {
-    // draw ground and player
-    background(groundColor);
-    fill(champColor);
-    ellipse(width / 2, height / 2, 50);
-
-    // magic here
-    //usingBlendMode();
-    usingErase();
-
-    // show overlay on top of game
-    image(overlay, 0, 0);
+    if (useBlendMode) {
+        drawUseBlendMode();
+    } else {
+        drawUseErase();
+    }
 }
 
-function usingErase() {
+function mousePressed() {
+    useBlendMode = !useBlendMode;
+
+    // reset blend mode
+    overlay.blendMode(BLEND);
+}
+
+// ---------------- ERASE -----------------
+function drawUseErase() {
+    // draw ground and things
+    background(groundColor);
+    drawThings();
+
     // clear overlay with overlay color
     overlay.clear();
     overlay.background(overlayColor);
 
-    // remove color at mouse
+    // magic here
+    fovUsingErase(mouseX, mouseY, fovRadius);
+
+    // show overlay on top of ground
+    image(overlay, 0, 0);
+
+    // show fps
+    fill("white");
+    text("click to switch mode", 10, height - 15);
+    text("FPS: " + ~~frameRate() + " - ERASE", 10, 15);
+}
+
+function fovUsingErase(x, y, d) {
     overlay.erase();
     overlay.fill(255);
-    overlay.ellipse(mouseX, mouseY, fovRadius);
+    overlay.ellipse(x, y, d);
     overlay.noErase();
 }
 
-function usingBlendMode() {
+// ---------------- BLEND MODE -----------------
+function drawUseBlendMode() {
+    // draw ground and things
+    background(groundColor);
+    drawThings();
+
     // clear overlay with overlay color
     overlay.clear();
     overlay.blendMode(BLEND);
     overlay.background(overlayColor);
 
-    // remove color at mouse
+    // magic here
+    fovUsingBlendMode(mouseX, mouseY, fovRadius);
+
+    // show overlay on top of ground
+    image(overlay, 0, 0);
+
+    // show fps
+    fill("white");
+    text("click to switch mode", 10, height - 15);
+    text("FPS: " + ~~frameRate() + " - BLEND MODE", 10, 15);
+}
+
+function fovUsingBlendMode(x, y, d) {
     overlay.blendMode(REMOVE);
     overlay.fill(255);
-    overlay.ellipse(mouseX, mouseY, fovRadius);
+    overlay.ellipse(x, y, d);
 }
+
+// -------------- HELPER ---------------
+function drawThings() {
+    stroke(30);
+    strokeWeight(2);
+
+    for (let t of things) {
+        fill(t.color);
+        ellipse(t.pos.x, t.pos.y, t.r * 2);
+
+        t.pos.add(t.vel);
+
+        if (t.pos.x < 0 || t.pos.x > width) t.vel.x *= -1;
+        if (t.pos.y < 0 || t.pos.y > height) t.vel.y *= -1;
+
+        t.pos.x = constrain(t.pos.x, 0, width);
+        t.pos.y = constrain(t.pos.y, 0, height);
+    }
+}
+
+// -------------- TEST ------------------
+// overlay.fill(255, 50);
+// overlay.ellipse(x, y, d);
+// overlay.ellipse(x, y, d / 1.25);
+// overlay.ellipse(x, y, d / 1.5);
+// overlay.ellipse(x, y, d / 1.75);
+// overlay.ellipse(x, y, d / 2);
