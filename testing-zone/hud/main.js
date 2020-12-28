ffc5bc5d3d64cb9594435732268bfc354de5597e
HoangTran <99.hoangtran@gmail.com>
2020-12-28 10:38:45 +0700

    [Testing-zone] hud

---
+++ testing-zone/hud/main.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+        .position(0, 0)
+        .parent("canvas-container");
+
+    textSize(30);
+    textAlign(CENTER, CENTER);
+    fill("white");
+}
+
+function draw() {
+    background(30);
+
+    text(
+        "Không dùng html để render HUD nữa, render bằng p5js luôn!!\nAi giỏi css html làm dùm thì xin cám ơn :)))",
+        width / 2,
+        height / 2
+    );
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight, true);
+}
